e7eb5700d836c21f223d71ab2c60f47a1635d04b
Chris Taylor <christa@datamatters.co.uk>
2018-11-27 13:39:50 +0000

    Initial Commit

---
+++ src/tradinggamecontrol/TradingGameControl.java
@@ -0,0 +1,13 @@
+package tradinggamecontrol;
+
+/**
+ *
+ * @author Chris Taylor - crtaylor.co.uk
+ */
+public class TradingGameControl {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
