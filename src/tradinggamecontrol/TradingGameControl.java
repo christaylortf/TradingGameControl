251cc25b145e1efad9b7b51df6b4e278f026f9f0
Chris Taylor <christa@datamatters.co.uk>
2018-11-27 14:11:08 +0000

    Started on main menu

--- src/tradinggamecontrol/TradingGameControl.java
+++ src/tradinggamecontrol/TradingGameControl.java
@@ -1,5 +1,7 @@
 package tradinggamecontrol;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Chris Taylor - crtaylor.co.uk
@@ -7,7 +9,45 @@ package tradinggamecontrol;
 public class TradingGameControl {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        menu();
     }
     
+    public static void menu() {
+      
+        System.out.println("=== World Trading Game ===");
+        printMenu();
+        Scanner scan = new Scanner(System.in);
+        int choice = scan.nextInt();
+        
+        switch(choice) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4: 
+                break;
+            case 0:
+                System.out.println("Bye!");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Not a valid option. Try again.");
+                menu();
+        }
+    }
+    
+    public static void printMenu() {
+        System.out.println("1 - Companies");
+        System.out.println("2 - Transactions");
+        System.out.println("3 - World Bank");
+        System.out.println("4 - Status");
+        System.out.println("0 - Exit");
+        
+    }
+    
+    public static void companiesMenu() {
+        System.out.println("");
+    }
 }
